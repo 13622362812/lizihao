10f037b22737f54b8fa6886509a7df0e97307f76
dudujerry452 <dudujerry452@qq.com>
2024-10-19 14:11:20 +0800

    zzycommit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
+int addnum(int a, int b){
+    return a+b;
+}
 int main(){
     cout<<"Hello, world !"<<endl;
     cout<<"lizihao hello world"<<endl;
     // And I add some notes here
     cout << "zhouzhiyuan" << endl;
+
+    int a,b;
+    cin >> a;
+    cin >> b;
+    cout << addnum(a,b) << endl;
     return 0;
 }
