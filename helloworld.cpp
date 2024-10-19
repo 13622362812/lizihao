34fe1f65ae08f18cc17d79782237eb3546725073
lizihao <13655362812@163.com>
2024-10-19 14:12:16 +0800

    lizihao commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int l;
+    cin>>l;
+    cout<<l<<endl;
+    //haonan
     cout<<"Hello, world !"<<endl;
     cout<<"lizihao hello world"<<endl;
     // And I add some notes here
