ec5c1d9e0a03bcf61bc24645c34b2d3261a9752c
dudujerry452 <dudujerry452@qq.com>
2024-10-19 14:08:59 +0800

    a

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main(){
     cout<<"Hello, world !"<<endl;
-    cout<<"lizihao"<<endl;
+    cout<<"lizihao hello world"<<endl;
+    // And I add some notes here
     cout << "zhouzhiyuan" << endl;
     return 0;
 }
